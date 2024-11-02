7fbd60819193e31749ab29536b69bbe22d2ca6ba
aurelwidyanti <aurelwyt@gmail.com>
2024-11-02 11:55:42 +0700

    fix: Enable CarSeeder in DatabaseSeeder to seed car data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            // CarSeeder::class,
+            CarSeeder::class,
         ]);
     }
 }
