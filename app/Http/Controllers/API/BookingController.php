57a5bf431a4b671c4a2387d305ffff2877c48cac
aurelwidyanti <aurelwyt@gmail.com>
2025-01-13 17:10:29 +0700

    refactor: Order bookings by created_by in bookingsByUser method

--- app/Http/Controllers/API/BookingController.php
+++ app/Http/Controllers/API/BookingController.php
@@ -109,7 +109,7 @@ class BookingController extends Controller
      */
     public function bookingsByUser()
     {
-        $booking = Booking::with('user', 'car', 'address')->where('user_id', Auth::user()->id)->get();
+        $booking = Booking::with('user', 'car', 'address')->where('user_id', Auth::user()->id)->orderBy('created_by', 'desc')->get();
 
         return response()->json([
             'status' => 'success',
